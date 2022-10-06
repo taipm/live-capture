ae3afd29c95d1608bb69f29c539d831fbd1374ff
Phan Minh Tài <taipm.vn@gmail.com>
2022-10-06 13:42:20 +0700

    Update

--- Imager.py
+++ Imager.py
@@ -5,3 +5,6 @@ def getTextFromImage(fileName):
     text = pytesseract.image_to_string(Image.open(fileName),lang='vie')
     print(text)
     return text
+
+text = getTextFromImage("1.png")
+print(text)
